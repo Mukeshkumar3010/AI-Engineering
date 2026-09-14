7502ebc21e180b91a0095902800e6795ddea3bfd
Mukeshkumar3010 <45436302+Mukeshkumar3010@users.noreply.github.com>
2026-09-14 12:11:18 +0000

    Update Fabric database query workflow

--- fabdb.py
+++ fabdb.py
@@ -1,19 +1,38 @@
 import pandas as pd
-from ai_engineering.fabric_db import connect
+from ai_engineering.fabric_db import sql_connect, pbi_connect
 
 
-def execute_query(query):
-    with connect() as conn:
+def execute_sql_query(query):
+    with sql_connect() as conn:
         df = pd.read_sql(query, conn)
 
     return df
 
+def get_schema(table_name):
+    query = f"""
+        SELECT TABLE_SCHEMA,
+        TABLE_NAME,
+        COLUMN_NAME,
+        DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
+        WHERE TABLE_SCHEMA = 'SalesLT'
+        AND TABLE_NAME = '{table_name}'
+    """
+    schema = execute_sql_query(query)
+    return schema
 
-#if __name__ == "__main__":
-#    main()
-result = execute_query("SELECT TOP 5 [SalesOrderID],"
-      "[SalesOrderDetailID],"
-      "[OrderQty],"
-      "[ProductID],"
-      "[UnitPrice] FROM [SalesLT].[SalesOrderDetail]")
-print(result)
+def execute_dax_query(dax_query):
+    data = pbi_connect(dax_query)
+    rows = data["results"][0]["tables"][0]["rows"]
+    df = pd.DataFrame(rows)
+    
+    return df
+
+# result = get_schema("SalesOrderDetail")
+# print(result)
+
+# result = execute_query("SELECT TOP 5 [SalesOrderID],"
+#       "[SalesOrderDetailID],"
+#       "[OrderQty],"
+#       "[ProductID],"
+#       "[UnitPrice] FROM [SalesLT].[SalesOrderDetail]")
+# print(result)
